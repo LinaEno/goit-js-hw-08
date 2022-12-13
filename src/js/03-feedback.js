50d63cb9acae2242757d8891fd40e3710da795ce
Lina Budkova <linabudkova35@gmail.com>
2022-12-13 19:23:21 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,42 @@
+import throttle from 'lodash.throttle';
+
+
+const STORAGE_KEY = 'feedback-form-state';
+
+const refs = {
+    form: document.querySelector('.feedback-form'),
+    email: document.querySelector('.feedback-form input'),
+    message: document.querySelector('.feedback-form textarea'),
+    btn: document.querySelector('.feedback-form button'),
+}
+
+refs.form.addEventListener('submit', onFormSubmit);
+refs.form.addEventListener('input', throttle(onFormInput, 500));
+
+formInfo();
+
+
+function onFormSubmit(event) {
+    console.log({email: refs.email.value, message: refs.message.value});
+    event.preventDefault();
+    event.currentTarget.reset();
+    localStorage.removeItem(STORAGE_KEY);
+}
+
+function onFormInput() {
+    const formData = JSON.stringify({email: refs.email.value, message: refs.message.value})
+    localStorage.setItem(STORAGE_KEY, formData);
+}
+
+function formInfo() {
+    const saveInfo = JSON.parse(localStorage.getItem(STORAGE_KEY));
+
+    if (saveInfo) {
+        refs.email.value = saveInfo.email;
+        refs.message.value = saveInfo.message;
+    }
+}
+
+
+
 
