f5fe7df1ace8f8e80b6c144a0c52e7766f4ab15b
Lina Budkova <linabudkova35@gmail.com>
2022-12-13 16:15:08 +0200

    qweqwe

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,28 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Change code below this line
 
 console.log(galleryItems);
+
+const makeGalleryItems = items => {
+  const { preview, original, description } = items;
+  return `
+  <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>
+  `;
+};
+
+const galleryContainer = document.querySelector('.gallery');
+
+const galleryImages = galleryItems.map(makeGalleryItems).join('');
+
+galleryContainer.insertAdjacentHTML('afterbegin', galleryImages);
+
+let galleryDescription = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionPosition: 'bottom',
+    captionDelay: 250,
+});
